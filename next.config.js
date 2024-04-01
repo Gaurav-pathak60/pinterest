535bc6e911fcd8e133daf86bc130be071c92a478
gaurav <gauravpathak9058@gmail.com>
2024-04-01 09:47:08 +0530

    hii

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
     images:{
         domains:['lh3.googleusercontent.com','firebasestorage.googleapis.com']
     }
 }
 
-module.exports = nextConfig
+module.exports = nextConfig;
