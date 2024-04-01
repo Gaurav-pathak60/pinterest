a8ade1adc35e007af9db1be3bbda08f8e2c05fa9
gaurav <gauravpathak9058@gmail.com>
2024-04-01 10:01:23 +0530

    firebase

--- app/Shared/firebaseConfig.js
+++ app/Shared/firebaseConfig.js
@@ -7,13 +7,12 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDsVcT58u5fnibQqpN2sX_OEggbFp0bYqs",
-  authDomain: "tubeguruji-app.firebaseapp.com",
-  projectId: "tubeguruji-app",
-  storageBucket: "tubeguruji-app.appspot.com",
-  messagingSenderId: "887202519021",
-  appId: "1:887202519021:web:584d0b453269ff8df979e4",
-  measurementId: "G-GXD1PVGG7Z"
+  apiKey: process.env.FIREBASE,
+  authDomain: "blog-app-418604.firebaseapp.com",
+  projectId: "blog-app-418604",
+  storageBucket: "blog-app-418604.appspot.com",
+  messagingSenderId: "896564895782",
+  appId: "1:896564895782:web:c3a56e5fc5c5901b6f06e7"
 };
 
 // Initialize Firebase
