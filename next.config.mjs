ede0fb8009a69c817ee8d4f8deeaf4969f5e9487
gaurav <gauravpathak9058@gmail.com>
2024-04-01 09:35:04 +0530

    ll

--- next.config.mjs
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'next/dist/shared/lib/utils'
-
-const nextConfig = defineConfig({
-    images:{
-        domains:['lh3.googleusercontent.com','firebasestorage.googleapis.com']
-    }
-})
-
-export default nextConfig
