fd2f28cb5807a743de5a3351254803130c4d9692
gaurav <gauravpathak9058@gmail.com>
2024-04-01 14:35:18 +0530

    k

--- src/app/Shared/firebaseConfig.js
+++ src/app/Shared/firebaseConfig.js
@@ -17,6 +17,6 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-export const app = initializeApp(firebaseConfig);
+ const app = initializeApp(firebaseConfig);
 export const analytics = getAnalytics(app);
-
+export default app;
